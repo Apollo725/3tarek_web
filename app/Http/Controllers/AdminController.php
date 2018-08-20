821ff7ba73a3df8fd9d7499e719dac0c87c21084
Apollo725 <natashazaharova725@outlook.com>
2018-08-21 09:21:29 +1000

    delete admin value in login verification

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
     public function login(Request $request) {
         if($request -> isMethod('post')) {
             $data = $request -> input();
-            if(Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'admin' => '1'])) {
+            if(Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
                 return redirect('admin/dashboard');
             } else {
                 return redirect('admin') -> with('flash_message_error', 'Invalid Username or Password!');
